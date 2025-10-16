025bd13da6dbce0e79a25ae4ffff36cc69a04459
0e1arquitetos <141720410+0e1arquitetos@users.noreply.github.com>
2025-10-16 15:32:24 -0300

    Delete lib directory

--- lib/qr.ts
+++
@@ -1,5 +0,0 @@
-import QRCode from 'qrcode';
-
-export async function createSvgQr(content: string) {
-  return QRCode.toString(content, { type: 'svg', margin: 2, color: { dark: '#063c46', light: '#ffffff00' } });
-}
